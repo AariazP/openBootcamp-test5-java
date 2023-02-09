bf65bd505ee8bc59ecbde9f0268c4817c3d9b043
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2023-02-08 21:13:59 -0500

    first commit

---
+++ src/main/java/org/alejandroArias/application/Main.java
@@ -0,0 +1,9 @@
+package org.alejandroArias.application;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("int, long, double, boolean, String...");
+        
+    }
+}
